cbafff5093167f889fabde6601b2a9916553ad9a
longdt <longdt555@gmail.com>
2020-09-21 12:52:13 +0700

    Update DTO

--- StudyProgramManagement/StudyProgramManagement/Startup.cs
+++ StudyProgramManagement/StudyProgramManagement/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Lib.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,8 @@ namespace StudyProgramManagement
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddMvc();
+            services.AddDbContext<SPMDbContext>(item => item.UseSqlServer(Configuration.GetConnectionString("spm")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
